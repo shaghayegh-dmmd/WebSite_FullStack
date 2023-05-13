19d19a64d1ddf3874b6ab2c19c0cb2e89665a3ef
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-13 16:17:23 +0330

    Creat and Delete Article

--- Cr24.WebSite.BLL/WebSiteService.cs
+++ Cr24.WebSite.BLL/WebSiteService.cs
@@ -4,20 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Cr24.WebSite.DAL.Models;
+
 namespace Cr24.WebSite.BLL
 {
     class WebSiteService
     {
         #region Article
-
-        public bool DeleteFileAccess(long id)
+        public bool CreatArticle(User_Article fileData)
         {
+            try
+            {
+                var newFile = new User_Article
+                {
+                    Category = fileData.Category,
+                    CreationDate = DateTime.Now,
+                    Description = fileData.Description,
+                    Summary = fileData.Summary,
+                    Title = fileData.Title,
+                    ImageId = fileData.ImageId,
+                    TagId = fileData.TagId,
+
+
+                };
+
+                using (var db = new WebEntity())
+                {
+                    db.User_Article.Add(newFile);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteArticle(long id)
+        {
+            
             try
             {
                 using (var db = new WebEntity())
                 {
                     var contObj = db.User_Article.First(o => o.Id == id);
-                    db.AccessFileContents.Remove(contObj);
+                    db.User_Article.Remove(contObj);
                     db.SaveChanges();
                 }
                 return true;

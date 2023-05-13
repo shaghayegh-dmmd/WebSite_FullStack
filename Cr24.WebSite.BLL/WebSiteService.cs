75b7d4fed574bd2fb045aba71a462470b07916a2
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-13 16:07:21 +0330

    Creat Service

---
+++ Cr24.WebSite.BLL/WebSiteService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cr24.WebSite.DAL.Models;
+namespace Cr24.WebSite.BLL
+{
+    class WebSiteService
+    {
+        #region Article
+
+        public bool DeleteFileAccess(long id)
+        {
+            try
+            {
+                using (var db = new WebEntity())
+                {
+                    var contObj = db.User_Article.First(o => o.Id == id);
+                    db.AccessFileContents.Remove(contObj);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        #endregion
+    }
+}

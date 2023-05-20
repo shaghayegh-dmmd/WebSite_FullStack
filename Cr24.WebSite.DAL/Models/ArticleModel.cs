79a1587920a591266745d6982a38ee8de909a757
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-20 09:52:52 +0330

    Edite Models

--- Cr24.WebSite.DAL/Models/ArticleModel.cs
+++ Cr24.WebSite.DAL/Models/ArticleModel.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
 
 namespace Cr24.WebSite.DAL.Models
 {
-    [DataContract]
+   
    public class ArticleModel
     {
-        [DataMember]
+        
         public long Id { get; set; }
 
-        [DataMember]
+        
         public string Title { get; set; }
 
-        [DataMember]
+       
         public string Description { get; set; }
 
         [DataMember]
@@ -37,10 +36,10 @@ namespace Cr24.WebSite.DAL.Models
         [DataMember]
         public string TagId { get; set; }
 
-        [DataMember]
+        
         public byte[] FileContent { get; set; }
 
-        [DataMember]
+       
         public byte[] ImageContent { get; set; }
 
 
--- Cr24.WebSite.DAL/Models/AttachmentModel.cs
+++ Cr24.WebSite.DAL/Models/AttachmentModel.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
 
 namespace Cr24.WebSite.DAL.Models
 {
-    [DataContract]
     public class AttachmentModel
     {
-        [DataMember]
         public long Id { get; set; }
 
-        [DataMember]
+
         public byte[] FileContent { get; set; }
 
-        [DataMember]
+       
         public string FileName { get; set; }
 
-        [DataMember]
+       
         public DateTime? CreationDate { get; set; }
     }
 }

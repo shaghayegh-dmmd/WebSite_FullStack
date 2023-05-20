407df5848ad5542733421200978260f25faf0208
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-20 10:17:57 +0330

    Edite Model

--- Cr24.WebSite.DAL/Models/ContactUsModel.cs
+++ Cr24.WebSite.DAL/Models/ContactUsModel.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
 
 namespace Cr24.WebSite.DAL.Models
 {
-    [DataContract]
+   
     public class ContactUsModel
     {
-        [DataMember]
+        
         public long Id { get; set; }
 
-        [DataMember]
+        
         public string Email { get; set; }
 
-        [DataMember]
+       
         public string UserName { get; set; }
 
-        [DataMember]
+       
         public string TextContent { get; set; }
 
-        [DataMember]
+        
         public string Subject { get; set; }
     }
 }

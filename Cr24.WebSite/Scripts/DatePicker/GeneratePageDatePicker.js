8462145c90cc7ba86b9a458beb6b00798f317437
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-21 16:12:24 +0330

    Add lyout And Nesessery File

---
+++ Cr24.WebSite/Scripts/DatePicker/GeneratePageDatePicker.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $(".datePicker").datepicker({
+        dateFormat: "yy/mm/dd",
+        changeMonth: true,
+        changeYear: true
+    });
+
+    if (!Modernizr.inputtypes.date) {
+        $('.datePicker').datepicker({
+                dateFormat: "yy/mm/dd",
+                changeMonth: true,
+                changeYear: true
+            }
+        );
+    }
+
+    $(".datePicker").css("direction", "ltr");
+});

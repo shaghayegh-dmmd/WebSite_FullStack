805338a9c639e8b22962ccbf28f18e2e0fbd176a
shaghayegh <96984431+shaghayegh-dmmd@users.noreply.github.com>
2023-05-24 16:22:54 +0330

    add Enum Article Category

---
+++ Cr24.WebSite.DAL/Enum/ArticleCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cr24.WebSite.DAL.Enum
+{
+    public enum ArticleCategory
+    {
+        CreditScoring,
+        CreditRating
+    }
+
+    public static class ArticleCategoryHelper
+    {
+        public static string ToPersian(this ArticleCategory article)
+        {
+            switch (article)
+            {
+                case ArticleCategory.CreditScoring:
+                    return "اعتبارسنجی";
+                case ArticleCategory.CreditRating:
+                    return "رتبه بندی";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(article), article, null);
+            }
+        }
+    }
+}
